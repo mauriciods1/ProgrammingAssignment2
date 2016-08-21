cc451265b339dcb029ad79fc5d3a5dc961943488
mauriciods1 <mauricio.ds1@hotmail.com>
2016-08-21 20:44:37 -0300

    Add files via upload
    addition of any comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,12 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## it's very similar with the example gave in assignement
+#this function will implement four functions, 'set', 'get', 'getinverse' and 'setinverse'
+#'set' will change the initial entry matrix
+#'get' will return the matrix x
+#'getinverse' will return the inverse of the matrix x, which is initially NULL
+#'setinverse' will set minverse with the REAL inverse matrix. Differently from the
+#example given in the assignement where we can set any value.
 
 makeCacheMatrix <- function(x = matrix()) {
     minverse <- NULL
@@ -18,6 +23,10 @@ makeCacheMatrix <- function(x = matrix()) {
     
     list(set = set, get = get, getinverse = getinverse, setinverse = setinverse)
 }
+
+#in this function we'll verify if the inverse was already calculated
+#if yes, then we just return the inverse
+#if not, then we'll calculate and return it.
 
 cacheSolve <- function(x, ...) {
     m <- x$getinverse()
